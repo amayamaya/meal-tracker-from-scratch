12eaff52dc101f0babb6f2d165a944ebcfbb467c
‘AMAYA <aamaya547@gmail.com>
2022-05-13 14:23:23 -0700

    switching back to original branch

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-  // import functions and grab DOM elements
+// import functions and grab DOM elements
 import { renderIngredients } from './utils.js';
-const form = document.getElementById("form");
-const ingredientEl = document.getElementById("ingredient");
-const quantityEl = document.getElementById("quantity");
-const measurementEl = document.getElementById("measurement");
-const ingredientListEl = document.getElementById("ingredient-list");
+
+const form = document.getElementById('form');
+const ingredientEl = document.getElementById('ingredient');
+const quantityEl = document.getElementById('quantity');
+const measurementEl = document.getElementById('measurement');
+const ingredientListEl = document.getElementById('ingredient-list');
 
 let ingredients = [];
 
-  // set event listeners 
+// set event listeners
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    // console.log('click');
     const ingredientName = ingredientEl.value;
     const ingredientQuantity = quantityEl.value;
     const ingredientMeasurement = measurementEl.value;
@@ -21,9 +23,22 @@ form.addEventListener('submit', (e) => {
         measurement: ingredientMeasurement,
     };
     ingredients.push(ingredient);
-    console.log(ingredients);
+    // console.log(ingredients)- works!;
+    renderIngredients();
+    //reset inputs
+    form.requestFullscreen();
+});
+// NEXT STEPS
+// displayIngredients
+function displayIngredients() {
+    // 1. clear the DOM (ingredientListEl DOM)
+    ingredientListEl.textContent = '';
+    ingredientEl.textContent = '(input.value)';
+    // 2. Loop through new state
+    let ingredients = [];
+    // 2A. Render LI for each item
 
-}); 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+    // 2B. Append each LI to UL
+}
+
+displayIngredients();
--- utils.js
+++ utils.js
@@ -1,12 +1,12 @@
 export function renderIngredients(item) {
     const li = document.createElement('li');
-    // console.log(item);
-    li.textContent = `${item.ingredient} ${item.quantity} ${item.measurement}`;
+    console.log(item);
+    li.textContent = `${item.name}, ${item.quantity}, ${item.measurement},`;
     return li;
 }
 
 export function displayIngredients(item) {
     const li = document.createElement('li');
-    li.textContent = `${item.ingredient} ${item.quantity} ${item.measurement}`;
+    li.textContent = `${item.name} ${item.quantity} ${item.measurement}`;
     return li;
 }
